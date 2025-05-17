9b3ef83e8375ebf1fab99f4ed3bf6314b2b99097
meedeley <nicholasaputra77@gmail.com>
2025-05-17 20:42:32 +0700

    feat : cors feature

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,32 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-
-
 Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+    Route::redirect('settings', 'settings/profile')->name('settings.redirect');
 
-    Route::get('/report-types', App\Livewire\ReportType\Index::class);
-    Route::get('/report-status', App\Livewire\ReportStatus\Index::class);
-    Route::get('/categories', App\Livewire\Categories\Index::class);
+    Route::get('/report-types', App\Livewire\ReportType\Index::class)
+        ->name('report.types');
+    Route::get('/report-status', App\Livewire\ReportStatus\Index::class)
+        ->name('report.status');
+    Route::get('/categories', App\Livewire\Categories\Index::class)
+        ->name('categories');
 
-    Route::get('/posts', App\Livewire\Post\Index::class)->name('posts.index');
-    Route::get('/posts/create', App\Livewire\Post\Create::class)->name('posts.create');
-    Route::get('/posts/edit/{slug}', App\Livewire\Post\Edit::class)->name('posts.edit');
-    Route::get('/posts/show/{slug}', App\Livewire\Post\Detail::class)->name('posts.show');
+    Route::get('/posts', App\Livewire\Post\Index::class)
+        ->name('posts.index');
+    Route::get('/posts/create', App\Livewire\Post\Create::class)
+        ->name('posts.create');
+    Route::get('/posts/edit/{slug}', App\Livewire\Post\Edit::class)
+        ->name('posts.edit');
+    Route::get('/posts/show/{slug}', App\Livewire\Post\Detail::class)
+        ->name('posts.show');
 
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    Route::get('settings/profile', Profile::class)
+        ->name('settings.profile');
+    Route::get('settings/password', Password::class)
+        ->name('settings.password');
+    Route::get('settings/appearance', Appearance::class)
+        ->name('settings.appearance');
 });
+
 
 require __DIR__.'/auth.php';
